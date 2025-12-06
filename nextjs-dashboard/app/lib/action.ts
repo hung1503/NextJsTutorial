4c1fa33864f82bfb876b6a418d3b1a4ae2c1ca1c
hung1503 <hungnguyen150300@gmail.com>
2025-12-06 12:09:36 +0200

    Chapter 12

--- nextjs-dashboard/app/lib/action.ts
+++ nextjs-dashboard/app/lib/action.ts
@@ -26,11 +26,16 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
-
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
-
+  } catch (error) {
+    console.error("Error creating invoice:", error);
+    return {
+      message: "There was an error creating the invoice.",
+    };
+  }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
@@ -43,12 +48,17 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
 
   const amountInCents = amount * 100;
-
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}`;
-
+  } catch (error) {
+    console.error("Error updating invoice:", error);
+    return {
+      message: "There was an error updating the invoice.",
+    };
+  }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
@@ -56,5 +66,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 export async function deleteInvoice(id: string) {
   await sql`
     DELETE FROM invoices WHERE id = ${id}`;
+
   revalidatePath("/dashboard/invoices");
 }
